59650014abd5ac3d441af271c4db6965cd9890ed
gobeumsu <gobeumsu@gmail.com>
2024-09-04 14:06:54 +0100

    feat : main.ts 기본 설정

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,24 @@
+import { Plugin } from "obsidian";
+import { AutoClassifierSettings, DEFAULT_SETTINGS, AutoClassifierSettingTab } from "./settings";
 
 export default class AutoClassifierPlugin extends Plugin {
+	settings: AutoClassifierSettings;
+
+	async onload() {
+		await this.loadSettings();
+
+		this.addSettingTab(new AutoClassifierSettingTab(this.app, this));
+
+
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+
+
 }
