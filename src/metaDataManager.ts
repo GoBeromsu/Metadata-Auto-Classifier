195bdc354edd9d18b54dc133fa290437dd4757d1
gobeumsu <gobeumsu@gmail.com>
2024-09-04 15:22:33 +0100

    feat : meta Data manager 추가

---
+++ src/metaDataManager.ts
@@ -0,0 +1,52 @@
+import { App, TFile } from "obsidian";
+
+export class MetaDataManager {
+	private app: App;
+
+	constructor(app: App) {
+		this.app = app;
+	}
+
+	async insertToFrontMatter(
+		file: TFile,
+		key: string,
+		value: string | string[],
+		overwrite = false
+	): Promise<void> {
+		await this.app.fileManager.processFrontMatter(file, (frontmatter) => {
+			frontmatter = frontmatter || {};
+
+			if (frontmatter[key] && !overwrite) {
+				if (Array.isArray(frontmatter[key])) {
+					if (Array.isArray(value)) {
+						frontmatter[key].push(...value);
+					} else {
+						frontmatter[key].push(value);
+					}
+				} else {
+					if (Array.isArray(value)) {
+						frontmatter[key] = [frontmatter[key], ...value];
+					} else {
+						frontmatter[key] = [frontmatter[key], value];
+					}
+				}
+			} else {
+				frontmatter[key] = value;
+			}
+		});
+	}
+
+	async getFrontMatter(file: TFile): Promise<Record<string, any> | null> {
+		const frontmatter =
+			this.app.metadataCache.getFileCache(file)?.frontmatter;
+		if (frontmatter) {
+			const { position, ...rest } = frontmatter;
+			return rest;
+		}
+		return null;
+	}
+
+	preprocessTags(tags: string[]): string[] {
+		return tags.map((tag) => tag.replace(/\s+/g, "_").replace(/^#/, ""));
+	}
+}
