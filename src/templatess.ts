9c2aff29b565ecf5bd7824d257004869371b3ed4
gobeumsu <gobeumsu@gmail.com>
2024-09-04 15:42:42 +0100

    feat : templates for prompt

---
+++ src/templatess.ts
@@ -0,0 +1,19 @@
+export const DEFAULT_CHAT_ROLE = `You are a JSON answer bot. Don't answer other words.`;
+export const DEFAULT_PROMPT_TEMPLATE = `Classify this content:
+"""
+{{input}}
+"""
+Answer format is JSON {reliability:0~1, output:selected_category}. 
+Even if you are not sure, qualify the reliability and select one. 
+Output must be one of these:
+
+{{reference}}
+`;
+
+export const DEFAULT_PROMPT_TEMPLATE_WO_REF = `Classify this content:
+"""
+{{input}}
+"""
+Answer format is JSON {reliability:0~1, output:selected_category}. 
+Even if you are not sure, qualify the reliability and recommend a proper category.
+`;
