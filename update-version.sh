37c73d1d717d1a1b9065900bbbc4d68efbd66fce
gobeumsu <gobeumsu@gmail.com>
2024-09-28 15:51:37 +0100

    fix : update sript syntax error

--- update-version.sh
+++ update-version.sh
@@ -5,16 +5,20 @@ echo "Enter new version number: "
 read VERSION
 
 # Update manifest.json
-sed -i 's/"version": ".*"/"version": "'$VERSION'"/' manifest.json
+sed -i '' 's/"version": ".*"/"version": "'$VERSION'"/' manifest.json
+
+# Update package.json
+sed -i '' 's/"version": ".*"/"version": "'$VERSION'"/' package.json
 
 # Update versions.json
 MINAPP=$(grep '"minAppVersion"' manifest.json | sed 's/.*: "\(.*\)".*/\1/')
 jq '. + {"'$VERSION'": "'$MINAPP'"}' versions.json > versions.json.tmp && mv versions.json.tmp versions.json
 
 # Commit changes
-git add manifest.json versions.json
+git add manifest.json versions.json package.json
 git commit -m "Bump version to $VERSION"
 
 # Create and push tag
 git tag $VERSION
-git push origin $VERSION
+git push origin --tags
+git push
