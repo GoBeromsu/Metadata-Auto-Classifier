90395de2e331fc05c7d58b1affcd2e3f8d857685
gobeumsu <gobeumsu@gmail.com>
2024-11-06 12:31:32 +0000

    refactor : optimize the default provider

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -14,12 +14,6 @@ export enum AIProvider {
 	Custom = 'Custom',
 }
 
-export enum DefaultProvider {
-	NAME = 'OpenAI',
-	BASE_URL = 'https://api.openai.com/v1',
-}
-// OpenAI model names
-
 export enum OpenAIModelName {
 	GPT_3_5_TURBO = 'gpt-3.5-turbo',
 	GPT_4_OMNI = 'gpt-4o',
@@ -27,9 +21,9 @@ export enum OpenAIModelName {
 }
 // Default OpenAI provider configuration
 const DEFAULT_OPENAI_PROVIDER: ProviderConfig = {
-	name: DefaultProvider.NAME,
+	name: AIProvider.OpenAI,
 	apiKey: '',
-	baseUrl: DefaultProvider.BASE_URL,
+	baseUrl: 'https://api.openai.com/v1',
 	models: [
 		{ name: OpenAIModelName.GPT_3_5_TURBO },
 		{ name: OpenAIModelName.GPT_4_OMNI },
