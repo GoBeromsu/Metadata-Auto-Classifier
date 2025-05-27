4171b1d446cb1287296c942dc400de3a79dbc202
gobeumsu <gobeumsu@gmail.com>
2025-05-27 21:47:35 +0100

    feat : add Jest configuration

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+const config = {
+	preset: 'ts-jest',
+	testEnvironment: 'node',
+	roots: ['<rootDir>/__tests__'],
+	moduleFileExtensions: ['ts', 'js', 'json'],
+	transform: {
+		'^.+\\.ts$': 'ts-jest',
+	},
+	moduleNameMapper: {
+		'^obsidian$': '<rootDir>/__mocks__/obsidian.ts',
+		'^api$': '<rootDir>/src/api',
+		'^utils/(.*)$': '<rootDir>/src/utils/$1',
+	},
+	testMatch: ['**/*.test.ts'],
+};
+export default config;
